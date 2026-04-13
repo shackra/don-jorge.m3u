92a40d7704ca6b1d7bfb0f8c00fb5c9a09433e39
Jorge Javier Araya Navarro <jorge@esavara.cr>
2026-04-12 23:18:10 -0600

    nixos: simplify module structure, fix lambda issue

--- nixos/default.nix
+++ nixos/default.nix
@@ -3,7 +3,7 @@
 with lib;
 
 let
-  channelSubmodule =
+  channelModule =
     { ... }:
     {
       options = {
@@ -18,7 +18,7 @@ let
         headers = mkOption {
           type = types.attrsOf types.str;
           default = { };
-          description = "HTTP headers to inject when proxying (e.g., Origin, Referer)";
+          description = "HTTP headers to inject when proxying";
         };
         logo = mkOption {
           type = types.str;
@@ -32,94 +32,64 @@ let
         };
       };
     };
-
-  mkServiceModule =
-    { packages }:
-    { config, ... }:
-
-    let
-      cfg = config.services.don-jorge;
-    in
-    {
-      config = mkIf cfg.enable {
-        systemd.services.don-jorge = {
-          description = "Don Jorge M3U Playlist Proxy";
-          wantedBy = [ "multi-user.target" ];
-
-          serviceConfig = {
-            Type = "simple";
-            Restart = "always";
-            RestartSec = "5s";
-
-            ExecStart =
-              let
-                channelToYAML =
-                  ch:
-                  let
-                    base = {
-                      name = ch.name;
-                      url = ch.url;
-                    };
-                    withHeaders = if ch.headers == { } then base else base // { headers = ch.headers; };
-                    withLogo = if ch.logo == "" then withHeaders else withHeaders // { logo = ch.logo; };
-                    withGroup = if ch.group == "" then withLogo else withLogo // { group = ch.group; };
-                  in
-                  withGroup;
-                channelsYaml = packages.writeText "channels.yaml" (
-                  generators.toYAML { } {
-                    channels = map channelToYAML cfg.channels;
-                  }
-                );
-              in
-              "${cfg.package}/bin/don-jorge -channels ${channelsYaml} -addr ${cfg.address}";
-          };
-        };
-      };
-    };
 in
-
 {
-  options = {
-    services.don-jorge = {
-      enable = mkEnableOption "Don Jorge M3U playlist proxy service";
+  options.services.don-jorge = {
+    enable = mkEnableOption "Don Jorge M3U playlist proxy service";
 
-      package = mkOption {
-        type = types.package;
-        defaultText = "pkgs.don-jorge";
-        description = "Don Jorge package to use";
-      };
+    package = mkOption {
+      type = types.package;
+      defaultText = "pkgs.don-jorge";
+      description = "Don Jorge package to use";
+    };
 
-      address = mkOption {
-        type = types.str;
-        default = ":8080";
-        description = "Address and port to listen on";
-      };
+    address = mkOption {
+      type = types.str;
+      default = ":8080";
+      description = "Address and port to listen on";
+    };
 
-      channels = mkOption {
-        type = types.listOf (types.submodule channelSubmodule);
-        default = [ ];
-        description = "List of TV channels to serve";
-        example = literalExpression ''
-          [
-            {
-              name = "Teletica";
-              url = "https://cdn01.teletica.com/stream/playlist.m3u8";
-              headers = {
-                Origin = "https://bradmax.com";
-                Referer = "https://bradmax.com";
-              };
-              logo = "https://example.com/teletica.png";
-              group = "Costa Rica";
-            }
-            {
-              name = "Opa TV";
-              url = "https://example.com/stream/playlist.m3u8";
-            }
-          ]
-        '';
-      };
+    channels = mkOption {
+      type = types.listOf (types.submodule channelModule);
+      default = [ ];
+      description = "List of TV channels to serve";
     };
   };
 
-  config = { config, pkgs, ... }: mkServiceModule { inherit pkgs; } { inherit config; };
+  config =
+    { config, pkgs, ... }:
+    let
+      cfg = config.services.don-jorge;
+    in
+    mkIf cfg.enable {
+      systemd.services.don-jorge = {
+        description = "Don Jorge M3U Playlist Proxy";
+        wantedBy = [ "multi-user.target" ];
+
+        serviceConfig = {
+          Type = "simple";
+          Restart = "always";
+          RestartSec = "5s";
+
+          ExecStart =
+            let
+              makeChannel =
+                ch:
+                (removeAttrs ch [
+                  "logo"
+                  "group"
+                ])
+                // (optionalAttrs (ch.headers != { }) { headers = ch.headers; })
+                // (optionalAttrs (ch.logo != "") { logo = ch.logo; })
+                // (optionalAttrs (ch.group != "") { group = ch.group; });
+              channelsYaml = pkgs.writeText "channels.yaml" (
+                generators.toYAML { } {
+                  channels = map makeChannel cfg.channels;
+                }
+              );
+            in
+            "${cfg.package}/bin/don-jorge -channels ${channelsYaml} -addr ${cfg.address}";
+        };
+      };
+    };
 }
